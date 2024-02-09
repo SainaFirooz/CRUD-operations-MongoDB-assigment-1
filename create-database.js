47dc5f9c3279578049da24e53454e679a3f5ed43
Saina <saina.firooz@gmail.com>
2024-02-09 09:43:23 +0100

    movie Schema

--- create-database.js
+++ create-database.js
@@ -3,3 +3,13 @@ import mongoose, {connect} from "mongoose";
 await connect("mongodb://127.0.0.1:27017/saina-assignment1");
 const {db} = mongoose.connection;
 
+const movieSchema = mongoose.Schema(
+    {
+        title: String,
+        director: String,
+        releaseYear: Number,
+        genres: [String],
+        ratings: [Number],
+        cast: [String]
+    }
+)
