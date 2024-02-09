eca4f5dd49c3f9c139f8dc5c8a869e1280de0979
Saina <saina.firooz@gmail.com>
2024-02-09 10:11:03 +0100

    mongoose

--- create-database.js
+++ create-database.js
@@ -1,15 +1,21 @@
 import mongoose, {connect} from "mongoose";
 
-await connect("mongodb://127.0.0.1:27017/saina-assignment1");
+const con = await connect("mongodb://localhost:27017/saina-assignment1");
 const {db} = mongoose.connection;
 
 const movieSchema = mongoose.Schema(
     {
-        title: String,
-        director: String,
-        releaseYear: Number,
-        genres: [String],
-        ratings: [Number],
-        cast: [String]
+        title: {type: String},
+        director: {type: String},
+        releaseYear: {type: Number},
+        genres: {type: [String]},
+        ratings: {type: [Number]},
+        cast: {type: [String]}
     }
-)
+);
+
+const movieModel = mongoose.model("Movie", movieSchema);
+
+const movieCol = await db.createCollection(movieModel);
+
+mongoose.connection.close();
