729ea34f25d2a81790e9af46bc324411e689b9b0
Saina <saina.firooz@gmail.com>
2024-02-11 21:54:46 +0100

    console log menu

--- main.js
+++ main.js
@@ -2,4 +2,11 @@
 
 import prompt from "prompt-sync";
 
-let input = prompt("");
+const p = prompt();
+
+console.log("Menu");
+console.log("1. Show all users.");
+console.log("2. Add new user-");
+console.log("3. Update existing user.");
+console.log("4. Delete user");
+console.log("5. Exist menu.");
