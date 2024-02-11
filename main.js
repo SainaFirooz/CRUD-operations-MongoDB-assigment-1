237eb459301abd6ecad5a20cf824f7fa412f135b
Saina <saina.firooz@gmail.com>
2024-02-11 22:03:58 +0100

    log change

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ import prompt from "prompt-sync";
 const p = prompt();
 
 console.log("Menu");
-console.log("1. Show all users.");
-console.log("2. Add new user-");
-console.log("3. Update existing user.");
-console.log("4. Delete user");
+console.log("1. Show all movies.");
+console.log("2. Add new movie.");
+console.log("3. Update existing movie.");
+console.log("4. Delete movie.");
 console.log("5. Exist menu.");
 
 let input = p("Make a choice by entering a number: ");
