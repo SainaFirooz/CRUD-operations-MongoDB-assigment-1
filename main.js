16c0425c55db5510a726006600ce6bc81ef1cd78
Saina <saina.firooz@gmail.com>
2024-02-12 13:47:48 +0100

    menu function with try catch, inquirer and switch

--- main.js
+++ main.js
@@ -2,30 +2,70 @@ import mongoose from "mongoose";
 import inquirer from "inquirer";
 
 const movieSchema = mongoose.Schema({
-    title: { type: String, required: true },
-    director: { type: String, required: true },
-    releaseYear: { type: Number, required: true },
-    genres: { type: [String], default: [] },
-    ratings: { type: [Number], default: [] },
-    cast: { type: [String], default: [] },
+  title: { type: String, required: true },
+  director: { type: String, required: true },
+  releaseYear: { type: Number, required: true },
+  genres: { type: [String], default: [] },
+  ratings: { type: [Number], default: [] },
+  cast: { type: [String], default: [] },
 });
 
 const movieModel = mongoose.model("Movies", movieSchema);
 
 const connectToDB = async () => {
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/saina-assignment1");
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB", error);
-    }
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/saina-assignment1");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+  }
 };
 
 const closeDBConnection = async () => {
-    try {
-        await mongoose.disconnect();
-        console.log("Disconnected from MongoDB");
-    } catch (error) {
-        console.error("Error disconnecting from MongoDB", error);
+  try {
+    await mongoose.disconnect();
+    console.log("Disconnected from MongoDB");
+  } catch (error) {
+    console.error("Error disconnecting from MongoDB", error);
+  }
+};
+
+const menu = async () => {
+  try {
+    while (true) {
+      const { choice } = await inquirer.prompt([
+        {
+          type: "list",
+          name: "choice",
+          message: "Menu",
+          choices: [
+            "View all movies",
+            "Add a new movies",
+            "Update a movie",
+            "Delete a movie",
+            "Exit",
+          ],
+        },
+      ]);
+      switch (choice) {
+        case "View all movies":
+          await viewAllMovies();
+          break;
+        case "Add a new movies":
+          await addNewMovie();
+          break;
+        case "Update a movie":
+          await updateMovie();
+          break;
+        case "Delete a movie":
+          await deleteMovie();
+          break;
+        case "Exit":
+          await closeDBConnection();
+          return;
+      }
     }
-}
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
