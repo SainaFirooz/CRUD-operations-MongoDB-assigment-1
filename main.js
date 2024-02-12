c07ac1c13f8c30e117e54c092a6eb68c193e9514
Saina <saina.firooz@gmail.com>
2024-02-12 12:10:10 +0100

    mocie schema with required and default

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 import mongoose from "mongoose";
 import inquirer from "inquirer";
+
+const movieSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    director: { type: String, required: true },
+    releaseYear: { type: Number, required: true },
+    genres: { type: [String], default: [] },
+    ratings: { type: [Number], default: [] },
+    cast: { type: [String], default: [] },
+});
