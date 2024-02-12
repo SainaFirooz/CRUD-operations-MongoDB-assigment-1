b1ad08fc2f9124e6413bcc446eb54e5f29079644
Saina <saina.firooz@gmail.com>
2024-02-12 11:52:37 +0100

    async function

--- create-database.js
+++ create-database.js
@@ -2,3 +2,10 @@ import mongoose, { connect } from "mongoose";
 
 const uri = "mongodb://127.0.0.1:27017/saina-assignment1"
 
+async function main() {
+    try {
+        await mongoose.connect(uri);
+        console.log("Conneceted to MongoDB");
+
+    }
+}
